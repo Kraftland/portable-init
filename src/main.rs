89f613b52bf692a73dbc1f872874a79a3059ac48
Kimiblock Moe <kimiblock@icloud.com>
2026-06-29 21:40:35 +0800

    main(): call get_configurations

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,24 @@ mod seccomp;
 mod envs;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> std::process::ExitCode {
 	let (tx, rx) = mpsc::channel::<logger::LogMessage>(16);
 
 	let _ = tokio::spawn(logger::logg_worker(rx));
 
 
-	logger::log(&tx, logger::LogLevel::Debug, "Hello, World".to_string());
+	let config_opts = envs::get_configurations();
+	let config_opts = match config_opts {
+		Ok(conf) => conf,
+		Err(e) => {
+			logger::log(&tx, logger::Loglevel::Fatal, format!("{e}:?"));
+			return std::process::ExitCode::FAILURE;
+		}
+	};
+
+	logger::log(&tx, logger::Loglevel::Debug, "Hello, World".to_string());
 	std::thread::sleep(std::time::Duration::from_secs(5));
+
+
+	return std::process::ExitCode::SUCCESS
 }
