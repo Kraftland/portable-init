11857ac5d754cea7b1942cc4a1598885d02803e7
Kimiblock Moe <kimiblock@icloud.com>
2026-06-30 16:57:06 +0800

    logger: don't output debug level if not specified or in debug build

--- src/logger.rs
+++ src/logger.rs
@@ -1,15 +1,43 @@
 use tokio;
 
+#[cfg(debug_assertions)]
+fn get_default_level() -> Loglevel {
+	Loglevel::Info
+}
+
+#[cfg(not(debug_assertions))]
+fn get_default_level() -> Loglevel {
+	Loglevel::Debug
+}
 
 pub async fn logg_worker(mut rx: tokio::sync::mpsc::Receiver<LogMessage>) {
+	let mut log_level = get_default_level();
+	let raw_os_level = std::env::var("PORTABLE_LOGGING");
+	match raw_os_level {
+		Ok(val)	=> {
+			if val == "debug" {
+				log_level = Loglevel::Debug;
+			}
+		}
+		Err(_)	=> {}
+	}
+
+
 	loop {
 		let request = rx.recv().await.unwrap();
 		match request.level {
 			Loglevel::Debug
-				=> println!(
-					"\x1b[38;2;125;241;118m[Init]\x1b[0m: {}",
-					request.message,
-				),
+				=> {
+					match log_level {
+						Loglevel::Debug	=> continue,
+						_		=> {}
+					};
+					println!(
+						"\x1b[38;2;125;241;118m[Init]\x1b[0m: {}",
+						request.message,
+					)
+				}
+
 			Loglevel::Info
 				=> println!(
 					"\x1b[38;2;119;222;250m[Init]\x1b[0m: {}",
