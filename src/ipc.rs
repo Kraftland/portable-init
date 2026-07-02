baf224d9b62974e67ed327b25613630dffc68481
Kimiblock Moe <kimiblock@icloud.com>
2026-07-02 15:35:17 +0800

    ipc: implement request_shutdown

--- src/ipc.rs
+++ src/ipc.rs
@@ -1,4 +1,5 @@
 use thiserror::Error;
+use zbus::proxy;
 
 #[derive(Debug, Error)]
 pub enum BusError {
@@ -7,15 +8,46 @@ pub enum BusError {
 
 	#[error("Failed shutting down bus connection: {0:#?}")]
 	ShutdownError(String),
+
+	#[error("Failed to create a bus proxy for {0}: {1:#?}")]
+	CreateProxyError(String, zbus::Error),
 }
 
-
+#[zbus::proxy(
+		default_path = "/top/kimiblock/portable/daemon",
+		interface = "top.kimiblock.Portable.Controller",
+)]
+trait Controller {
+	fn stop(&self) -> zbus::Result<()>;
+}
 
 pub struct IPC {
-	connection: zbus::Connection
+	connection: zbus::Connection,
+	daemon_bus_name: String,
 }
 
 impl IPC {
+	pub async fn request_shutdown(self: &Self) -> Result<(), BusError> {
+		let dest = self.daemon_bus_name.clone();
+		let proxy = ControllerProxy::builder(&self.connection)
+			.destination(dest);
+		let proxy = match proxy {
+			Ok(val)	=>	val,
+			Err(e)	=>	return Err(BusError::CreateProxyError("Stop".into(), e))
+		};
+		let proxy = match proxy.build().await {
+			Ok(val)	=>	val,
+			Err(e)	=>	return Err(BusError::CreateProxyError("Stop".into(), e))
+		};
+		let reply = proxy.0.call_noreply("Stop", &());
+		match reply.await {
+			Ok(_)	=> Ok(()),
+			Err(e)	=> Err(
+				BusError::ShutdownError(format!("{e:#?}"))
+			)
+		}
+	}
+
 	pub async fn connect(conf: &crate::envs::ConfigOpts) -> Result<Self, BusError> {
 		let conn = zbus::connection::Builder::session();
 		let conn = match conn {
@@ -33,8 +65,15 @@ impl IPC {
 
 		let conn = conn.allow_name_replacements(false);
 
+		let daemon_name = format!("top.kimiblock.portable.{}", conf.sandbox_id);
+
 		match conn.build().await {
-			Ok(val)	=> Ok(Self { connection: val }),
+			Ok(val)	=> Ok(
+				Self {
+					connection: val,
+					daemon_bus_name: daemon_name,
+				},
+			),
 			Err(e)	=> Err(BusError::ConnectError(e))
 		}
 	}
