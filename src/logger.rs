425490e2f0ba09922d8e098cff749065728a9265
Kimiblock Moe <kimiblock@icloud.com>
2026-07-05 23:17:31 +0800

    logger: drop async mode

--- src/logger.rs
+++ src/logger.rs
@@ -1,98 +1,37 @@
-use tokio;
+#[cfg(not(debug_assertions))]
+pub fn log_debug(_msg: String) {}
 
 #[cfg(debug_assertions)]
-fn get_default_level() -> Loglevel {
-	Loglevel::Info
+pub fn log_debug(msg: String) {
+	println!(
+		"\x1b[38;2;125;241;118m[Init]\x1b[0m: {}",
+		msg,
+	)
 }
 
-#[cfg(not(debug_assertions))]
-fn get_default_level() -> Loglevel {
-	Loglevel::Debug
+pub fn log_info(msg: String) {
+	println!(
+		"\x1b[38;2;119;222;250m[Init]\x1b[0m: {}",
+		msg,
+	)
 }
 
-pub async fn logg_worker(
-		mut rx: tokio::sync::mpsc::Receiver<LogMessage>,
-		cancel_token: tokio_util::sync::CancellationToken,
-	) {
-	let mut log_level = get_default_level();
-	let raw_os_level = std::env::var("PORTABLE_LOGGING");
-	match raw_os_level {
-		Ok(val)	=> {
-			if val == "debug" {
-				log_level = Loglevel::Debug;
-			}
-		}
-		Err(_)	=> {}
-	}
-
-
-	loop {
-		let request = tokio::select! {
-			_ = cancel_token.cancelled()	=> {return}
-			v = rx.recv()			=> {v}
-		};
-		let request = request.unwrap();
-		match request.level {
-			Loglevel::Debug
-				=> {
-					match log_level {
-						Loglevel::Debug	=> continue,
-						_		=> {}
-					};
-					println!(
-						"\x1b[38;2;125;241;118m[Init]\x1b[0m: {}",
-						request.message,
-					)
-				}
-
-			Loglevel::Info
-				=> println!(
-					"\x1b[38;2;119;222;250m[Init]\x1b[0m: {}",
-					request.message,
-				),
-			Loglevel::Warn
-				=> println!(
-					"\x1b[38;2;255;209;59m[Init]\x1b[0m: {}",
-					request.message,
-				),
-			Loglevel::Fatal
-				=> {
-					println!(
-						"\x1b[38;2;255;0;0m[Init]\x1b[0m: {}",
-						request.message,
-					);
-					cancel_token.cancel();
-					std::process::exit(1)
-				}
-		}
-	}
+pub fn log_warn(msg: String) {
+	println!(
+		"\x1b[38;2;255;209;59m[Init]\x1b[0m: {}",
+		msg,
+	)
 }
 
-#[derive(Debug)]
-pub struct LogMessage {
-	pub level:		Loglevel,
-	pub message:	String,
-}
-
-#[derive(Debug)]
-pub enum Loglevel {
-	Debug,
-	Info,
-	Warn,
-	Fatal,
-}
-
-pub async fn log(tx: &tokio::sync::mpsc::Sender<LogMessage>, level: Loglevel, msg: String) {
-	let tx_new = tx.clone();
-	tx_new.send(LogMessage{
-		level:		level,
-		message:	msg,
-	}).await.unwrap();
-}
-
-pub fn log_sync(tx: &tokio::sync::mpsc::Sender<LogMessage>, level: Loglevel, msg: String) {
-	tx.try_send(LogMessage{
-		level:		level,
-		message:	msg
-	}).unwrap();
+pub fn log_fatal(msg: String) {
+	println!(
+		"\x1b[38;2;255;0;0m[Init]\x1b[0m: {}",
+		msg,
+	);
+	nix::sys::signal::kill(
+		nix::unistd::Pid::this(),
+		nix::sys::signal::SIGTERM,
+	);
+	std::thread::sleep(std::time::Duration::from_secs(5));
+	panic!("Init did not terminate on error")
 }
