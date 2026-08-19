6f6cee7df058a5f94f4d4da66109a2553029c21c
Kimiblock Moe <kimiblock@icloud.com>
2026-08-19 21:44:35 +0800

    implement the systemd status update subsystem

--- src/counter/status/systemd.rs
+++ src/counter/status/systemd.rs
@@ -15,6 +15,27 @@ impl super::Init for SystemdStatus {
 	type StatusError = SystemdError;
 }
 
+impl super::UpdateStatus for SystemdStatus {
+	async fn update(&self, status: &super::SandboxStatus) -> Result<(), Self::StatusError> {
+		systemd::daemon::notify(
+			false,
+			vec![
+				(
+					systemd::daemon::STATE_STATUS,
+					&status
+						.to_string(),
+				)
+			].iter(),
+		)
+			.map_err(SystemdError::NotifyError)
+			?;
+
+		Ok(())
+	}
+
+	type StatusError = SystemdError;
+}
+
 #[derive(thiserror::Error, Debug)]
 pub enum SystemdError {
 	#[error("Error sending systemd notification: {0:#?}")]
