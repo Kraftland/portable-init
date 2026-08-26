9c0f70d5d9e6c15b09b2c54df872fe230107f694
Kimiblock Moe <kimiblock@icloud.com>
2026-08-26 11:38:54 +0800

    split unotify into a module

--- src/seccomp.rs
+++ src/seccomp.rs
@@ -1,6 +1,8 @@
 use thiserror::Error;
 
 mod list;
+mod unotify;
+pub use unotify::process_seccomp_unotify;
 
 pub use list::compile_syscall_list;
 
@@ -30,89 +32,6 @@ pub struct SyscallList {
 	pub allow_list: Vec<libseccomp::ScmpSyscall>,
 	pub debug_list: Vec<libseccomp::ScmpSyscall>,
 	pub selective: Vec<libseccomp::ScmpSyscall>, // depends on lockdown
-}
-
-pub fn process_seccomp_unotify (
-	fd: libseccomp::ScmpFd,
-	cancel_token: tokio_util::sync::CancellationToken,
-) {
-
-	let errno_raw = - {
-		use nix::errno::Errno;
-		let err = Errno::ENOSYS;
-		err as i32
-	};
-
-	let fake_allow: Vec<String> = vec![
-		"chroot".into(),
-		"capset".into(),
-		"setfsuid".into(),
-	];
-
-	loop {
-		let request = libseccomp::ScmpNotifReq::receive(fd);
-		if cancel_token.is_cancelled() {
-			return
-		}
-		let request = match request {
-			Ok(val)	=> val,
-			Err(e)	=> {
-				eprintln!("Could not receive seccomp notification: {e:#?}");
-				return
-			}
-		};
-
-		let syscall_name = request.data.syscall.get_name();
-		let syscall_name = match syscall_name {
-			Ok(val)	=> val,
-			Err(e)	=> {
-				eprintln!("Could not resolve syscall: {:#?}", e);
-				let response = libseccomp::ScmpNotifResp::new_continue(
-					request.id,
-					libseccomp::ScmpNotifRespFlags::empty(),
-				);
-				match response.respond(fd) {
-					Ok(_)	=> {}
-					Err(e)	=> {
-						eprintln!("Could not respond to syscall: {e:#?}");
-					}
-				};
-				continue;
-			}
-		};
-
-		crate::logger::log_warn(
-			format!(
-				"PID {} performed illegal syscall: {} on architecture {:?}",
-				&request.pid,
-				syscall_name,
-				&request.data.arch,
-			)
-		);
-
-		let response = {
-			if fake_allow.contains(&syscall_name) {
-				libseccomp::ScmpNotifResp::new_val(
-					request.id,
-					0,
-					libseccomp::ScmpNotifRespFlags::empty(),
-				)
-			} else {
-				libseccomp::ScmpNotifResp::new_error(
-					request.id,
-					errno_raw,
-					libseccomp::ScmpNotifRespFlags::empty(),
-				)
-			}
-		};
-
-		match response.respond(fd) {
-			Ok(_)	=> {},
-			Err(e)	=> {
-				eprintln!("Could not filter system call: {e:#?}")
-			},
-		}
-	}
 }
 
 pub async fn compile_filter (
